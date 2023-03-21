d2d80c9201281478f00dd3e8978ce250396e8862
ikedark <david.v1795@gmail.com>
2023-03-21 12:50:43 -0700

    cambios

--- app/src/main/java/mx/edu/potros/practica9/MainActivity.kt
+++ app/src/main/java/mx/edu/potros/practica9/MainActivity.kt
@@ -1,10 +1,12 @@
 package mx.edu.potros.practica9
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,5 +27,12 @@ class MainActivity : AppCompatActivity() {
 
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        val sign_in_button : SignInButton = findViewById(R.id.sign_in_button)
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
     }
 }
